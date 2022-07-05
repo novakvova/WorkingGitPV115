6fa6a4484facf009ffcb33630cfad2b6199d8d3b
Max6456 <102053595+Max6456@users.noreply.github.com>
2022-07-05 19:56:16 +0300

    Add files via upload
    add-hom2

---
+++ one_homework/one_homework/one_homework.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include "stocks.h"
+using namespace std;
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	cout.precision(2);
+	cout.setf(ios_base::fixed, ios_base::floatfield);
+	cout.setf(ios_base::showpoint);
+	cout << "Используются конструкторы для создания новых объектов \n";
+	Stock stock1("NanoSmart ", 12, 20.0);
+	stock1.show();
+	Stock stock2 = Stock(" Boffo Obj ects ", 2, 2.0);
+	stock2.show();
+	cout << " Присвоение stock1 stock2 : \n ";
+	stock2 = stock1;
+	cout << "Листинг stock1 и stock2 : \n ";
+	stock1.show();
+	stock2.show();
+	cout << " Применение конструктора для сброса объекта \n";
+	stock1 = Stock("Nifty Foods ", 10, 50.0);
+	cout << "Измененный stock1 : \n";
+	stock1.show();
+	cout << " Готово\ n";
+	return 0;
+}
---
+++ one_homework/one_homework/stocks.h
@@ -0,0 +1,25 @@
+#pragma once
+#pragma once
+#define STOCKl_Н_
+using namespace std;
+class Stock
+{
+private:
+	char company[30];
+	int shares;
+	double share_val;
+	double total_val;
+	void set_tot()
+	{
+		total_val = shares * share_val;
+	};
+public:
+	Stock();
+	Stock(const char* со, int n = 0, double pr = 0.0);
+	~Stock();
+	void buy(int num, double price);
+	void sell(int num, double price);
+	void update(double price);
+	void show();
+
+};
