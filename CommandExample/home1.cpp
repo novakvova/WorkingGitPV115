c86f595503b34570b3b1c4e2c4164b1ff7342d1d
yura5255 <bortnik2604@gmail.com>
2022-07-05 20:13:08 +0300

    1 - Home 1 Bortnik

---
+++ CommandExample/home1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Stock.h"
+#include "Windows.h"
+using namespace std;
+int main()
+{
+	SetConsoleOutputCP(1251);
+	cout << "Исполь зуются конструкторы для создания новых объектов \n";
+	Stock stock1("NanoSmart ", 12, 20.0);
+	stock1.show();
+	Stock stock2 = Stock(" Boffo Objects ", 2, 2.0);
+	stock2.show();
+	cout << " Присвоение stock1 stock2 : \n";
+	stock2 = stock1;
+	cout << "Листинг stock1 и stock2 : \n";
+	stock1.show();
+	stock2.show();
+	cout << " Применение конструктора для сброса объекта \n";
+	stock1 = Stock("Nifty Foods ", 10, 50.0);
+	cout << "Измененный stock1 : \n";
+	stock1.show();
+	cout << " Готово\n";
+	return 0;
+}
